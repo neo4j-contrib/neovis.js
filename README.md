a8b303384900f9905314785bbf302adf550db056
johnymontana <lyonwj@gmail.com>
2016-04-06 11:55:06 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# neovis.js
+
+Graph visualizations powered by vis.js with data from Neo4j.
+
+## Coming Soon!
---
+++ src/neovis.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var NeoVis = (function () {
+  function NeoVis(config) {
+    this._config = config;
+  }
+
+  return NeoVis;
+}());
