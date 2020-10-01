9ac9a397140742c2d275856c91187a86245acf6b
Chris Zirkel <chris@Chris-Mac.fritz.box>
2020-10-01 11:48:13 +0200

    change type definitions

--- index.d.ts
+++ index.d.ts
@@ -35,7 +35,33 @@ export interface INeovisConfig {
     trust?: "TRUST_ALL_CERTIFICATES" | "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES";
 }
 
+export interface INode {
+    group: string;
+    id: number;
+    label: string;
+    raw: any;
+    shape: string;
+    title: string;
+    value: number;
+}
+
+export interface IEdge {
+    from: number;
+    id: number;
+    label: string;
+    raw: any;
+    title: string;
+    to: number;
+    value: number;
+}
+
 declare class Neovis {
+    nodes: {
+        [id: number]: INode,
+    }
+    edges: {
+        [id: number]: IEdge,
+    }
     constructor(config: INeovisConfig);
     render(): void;
     clearNetwork(): void;
