8fffe33aaab22458fee585a133e390f4e6ec56b7
thebestnom <shoval.arad@gmail.com>
2021-02-20 23:20:46 +0200

    Fix for older browsers

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-	"presets": [["@babel/preset-env", {}]],
-	"plugins": [
-		["@babel/plugin-transform-runtime", {"corejs": 3}],
+	presets: [["@babel/preset-env", {
+		targets	: "defaults",
+		useBuiltIns: "usage",
+		corejs: 3
+	}]],
+	plugins: [
+		["@babel/plugin-transform-runtime", {corejs: 3}],
 		"@babel/plugin-proposal-class-properties"
 	]
 }
